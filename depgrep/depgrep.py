bf30f596f2527436e29bc58ab52c883b92d51534
Danny McDonald <mcddjx@gmail.com>
2019-06-11 13:35:14 +0200

    tests pass, remove python2 support

--- depgrep/depgrep.py
+++ depgrep/depgrep.py
@@ -113,12 +113,15 @@ macro definitions to ``m`` and initialises ``l`` to an empty dictionary.
 import functools
 import re
 
+from six import binary_type, text_type
+
 import pyparsing
 import nltk.tree
 
 
 class TgrepException(Exception):
     '''Tgrep exception type.'''
+
     pass
 
 
@@ -851,24 +854,18 @@ def _tgrep_exprs_action(_s, _l, tokens):
     return top_level_pred
 
 
-
-def _build_depgrep_parser(df=False, case_sensitive=False):
-    """
+def _build_tgrep_parser(set_parse_actions=True):
+    '''
     Builds a pyparsing-based parser object for tokenizing and
     interpreting tgrep search strings.
-    """
-    tgrep_op = pyparsing.Optional('!') + pyparsing.Regex('[$%,.<>&-\|\+][%,.<>0-9\-\':\|]*')
-    # match the node type info
-    node_attr = pyparsing.Optional(pyparsing.Word('siwlpnmfgdchSIWLPNMFGDCH', max=1))
-    # match regex nodes
-    tgrep_node_regex = node_attr + pyparsing.QuotedString(
-        quoteChar='/', escChar='\\', unquoteResults=False
-    )
-    # match string nodes
-    tgrep_qstring = node_attr + pyparsing.QuotedString(
+    '''
+    tgrep_op = pyparsing.Optional('!') + pyparsing.Regex('[$%,.<>][%,.<>0-9-\':]*')
+    tgrep_qstring = pyparsing.QuotedString(
         quoteChar='"', escChar='\\', unquoteResults=False
     )
-
+    tgrep_node_regex = pyparsing.QuotedString(
+        quoteChar='/', escChar='\\', unquoteResults=False
+    )
     tgrep_qstring_icase = pyparsing.Regex('i@\\"(?:[^"\\n\\r\\\\]|(?:\\\\.))*\\"')
     tgrep_node_regex_icase = pyparsing.Regex('i@\\/(?:[^/\\n\\r\\\\]|(?:\\\\.))*\\/')
     tgrep_node_literal = pyparsing.Regex('[^][ \r\t\n;:.,&|<>()$!@%\'^=]+')
@@ -911,47 +908,64 @@ def _build_depgrep_parser(df=False, case_sensitive=False):
         + tgrep_node_label.copy().setWhitespaceChars('')
     ) | tgrep_node_expr
     tgrep_node = tgrep_parens | (
-        pyparsing.Optional('\'') + tgrep_node_expr2 + pyparsing.ZeroOrMore('|' + tgrep_node_expr)
+        pyparsing.Optional("'")
+        + tgrep_node_expr2
+        + pyparsing.ZeroOrMore("|" + tgrep_node_expr)
     )
     tgrep_brackets = pyparsing.Optional('!') + '[' + tgrep_relations + ']'
     tgrep_relation = tgrep_brackets | (tgrep_op + tgrep_node)
     tgrep_rel_conjunction = pyparsing.Forward()
     tgrep_rel_conjunction << (
-        tgrep_relation + pyparsing.ZeroOrMore(pyparsing.Optional('&') + tgrep_rel_conjunction)
+        tgrep_relation
+        + pyparsing.ZeroOrMore(pyparsing.Optional('&') + tgrep_rel_conjunction)
     )
-    tgrep_relations << tgrep_rel_conjunction + pyparsing.ZeroOrMore('|' + tgrep_relations)
+    tgrep_relations << tgrep_rel_conjunction + pyparsing.ZeroOrMore(
+        "|" + tgrep_relations
+    )
     tgrep_expr << tgrep_node + pyparsing.Optional(tgrep_relations)
     tgrep_expr_labeled = tgrep_node_label_use + pyparsing.Optional(tgrep_relations)
     tgrep_expr2 = tgrep_expr + pyparsing.ZeroOrMore(':' + tgrep_expr_labeled)
-    macro_defn = pyparsing.Literal('@') + pyparsing.White().suppress() + macro_name + tgrep_expr2
+    macro_defn = (
+        pyparsing.Literal('@') + pyparsing.White().suppress() + macro_name + tgrep_expr2
+    )
     tgrep_exprs = (
         pyparsing.Optional(macro_defn + pyparsing.ZeroOrMore(';' + macro_defn) + ';')
         + tgrep_expr2
         + pyparsing.ZeroOrMore(';' + (macro_defn | tgrep_expr2))
         + pyparsing.ZeroOrMore(';').suppress()
     )
-
-    tgrep_node_label_use.setParseAction(_tgrep_node_label_use_action)
-    tgrep_node_label_use_pred.setParseAction(_tgrep_node_label_pred_use_action)
-    macro_use.setParseAction(_tgrep_macro_use_action)
-    tgrep_node.setParseAction(
-        lambda x, y, z: _tgrep_node_action(x, y, z, cols=df.columns, case_sensitive=case_sensitive)
-    )
-    tgrep_node_expr2.setParseAction(_tgrep_bind_node_label_action)
-    tgrep_parens.setParseAction(_tgrep_parens_action)
-    tgrep_nltk_tree_pos.setParseAction(_tgrep_nltk_tree_pos_action)
-    tgrep_relation.setParseAction(lambda x, y, z: _tgrep_relation_action(x, y, z, df=df))
-    tgrep_rel_conjunction.setParseAction(_tgrep_conjunction_action)
-    tgrep_relations.setParseAction(_tgrep_rel_disjunction_action)
-    macro_defn.setParseAction(_macro_defn_action)
-    # the whole expression is also the conjunction of two
-    # predicates: the first node predicate, and the remaining
-    # relation predicates
-    tgrep_expr.setParseAction(_tgrep_conjunction_action)
-    tgrep_expr_labeled.setParseAction(_tgrep_segmented_pattern_action)
-    tgrep_expr2.setParseAction(functools.partial(_tgrep_conjunction_action, join_char=':'))
-    tgrep_exprs.setParseAction(_tgrep_exprs_action)
+    if set_parse_actions:
+        tgrep_node_label_use.setParseAction(_tgrep_node_label_use_action)
+        tgrep_node_label_use_pred.setParseAction(_tgrep_node_label_pred_use_action)
+        macro_use.setParseAction(_tgrep_macro_use_action)
+        tgrep_node.setParseAction(_tgrep_node_action)
+        tgrep_node_expr2.setParseAction(_tgrep_bind_node_label_action)
+        tgrep_parens.setParseAction(_tgrep_parens_action)
+        tgrep_nltk_tree_pos.setParseAction(_tgrep_nltk_tree_pos_action)
+        tgrep_relation.setParseAction(_tgrep_relation_action)
+        tgrep_rel_conjunction.setParseAction(_tgrep_conjunction_action)
+        tgrep_relations.setParseAction(_tgrep_rel_disjunction_action)
+        macro_defn.setParseAction(_macro_defn_action)
+        # the whole expression is also the conjunction of two
+        # predicates: the first node predicate, and the remaining
+        # relation predicates
+        tgrep_expr.setParseAction(_tgrep_conjunction_action)
+        tgrep_expr_labeled.setParseAction(_tgrep_segmented_pattern_action)
+        tgrep_expr2.setParseAction(
+            functools.partial(_tgrep_conjunction_action, join_char=':')
+        )
+        tgrep_exprs.setParseAction(_tgrep_exprs_action)
     return tgrep_exprs.ignore('#' + pyparsing.restOfLine)
+
+
+def tgrep_tokenize(tgrep_string):
+    '''
+    Tokenizes a TGrep search string into separate tokens.
+    '''
+    parser = _build_tgrep_parser(False)
+    if isinstance(tgrep_string, binary_type):
+        tgrep_string = tgrep_string.decode()
+    return list(parser.parseString(tgrep_string))
 
 
 def tgrep_compile(tgrep_string):
@@ -963,17 +977,6 @@ def tgrep_compile(tgrep_string):
     if isinstance(tgrep_string, binary_type):
         tgrep_string = tgrep_string.decode()
     return list(parser.parseString(tgrep_string, parseAll=True))[0]
-
-def depgrep_compile(depgrep_string, df=False, case_sensitive=False):
-    """
-    Parses (and tokenizes, if necessary) a depgrep search string into a
-    lambda function.
-    """
-    df = df.drop(['file', 's', 'i'], axis=1, errors='ignore').reset_index()
-    parser = _build_depgrep_parser(df=df, case_sensitive=case_sensitive)
-    if isinstance(depgrep_string, bytes):
-        depgrep_string = depgrep_string.decode()
-    return list(parser.parseString(depgrep_string, parseAll=True))[0]
 
 
 def treepositions_no_leaves(tree):
