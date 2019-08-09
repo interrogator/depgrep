5a8aa6a4120387af18efca51aaee405a39e58354
Danny McDonald <mcddjx@gmail.com>
2019-08-09 13:59:29 +0200

    fix path

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ set -e
 echo "Doing $1 update"
 
 # use python 3.7 virtualenv
-source ~/venv/3.7/bin/activate
+source ~/venv/py3.7/bin/activate
 
 # check formatting
 flake8 depgrep/* tests/* setup.py
